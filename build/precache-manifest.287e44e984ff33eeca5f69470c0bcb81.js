cf883cb3b90a791a2f8b62ec63d8800e0a52a08b
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-06-14 22:32:29 -0700

    I ran npm run-script build, not sure what it did.

---
+++ build/precache-manifest.287e44e984ff33eeca5f69470c0bcb81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1de0ea194666aa3910d3efd6d72a5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef643f5ea0124095d19",
+    "url": "/static/js/2.30aa3212.chunk.js"
+  },
+  {
+    "revision": "6688e7be6160c1dc232d",
+    "url": "/static/js/main.f7800650.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
